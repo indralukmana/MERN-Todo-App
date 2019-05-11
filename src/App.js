3fff346437c357e4e925f5817ccc3a6c8b6fbfe2
Indra Lukmana <indralukmana@live.com>
2019-05-11 15:59:39 +0700

    Add create todo mechanism in the front-end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import TodosList from './components/todos-list.component';
+import CreateTodo from './components/create-todo.component';
+import EditTodo from './components/create-todo.component';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-class App extends Component{
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <Route path="/" exact component={TodosList} />
-          <Route path="/edit/:id" exact component={EditTodo} />
-          <Route path="/create" exact component={CreateTodo} />
-          <h2>MERN Todo App</h2>
-        </div>
-      </Router>
-    );
-  }
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div className='container'>
+					<nav className='navbar navbar-expand-lg navbar-light bg-light'>
+						<Link to='/' className='navbar-brand'>
+							MERN Todo
+						</Link>
+						<div className='collapse navbar-collapse'>
+							<ul className='navbar-nav mr-auto'>
+								<li className='navbar-item'>
+									<Link to='/' className='nav-link'>
+										Todos
+									</Link>
+								</li>
+								<li>
+									<Link to='/create' className='nav-link'>
+										Create
+									</Link>
+								</li>
+							</ul>
+						</div>
+					</nav>
+					<br />
+					<Route path='/' exact component={TodosList} />
+					<Route path='/edit/:id' exact component={EditTodo} />
+					<Route path='/create' exact component={CreateTodo} />
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
